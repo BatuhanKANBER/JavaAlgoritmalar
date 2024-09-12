1cf19c66a63311ff68ad435307e07c1a32df700c
Batuhan KANBER <batuhankanber28@gmail.com>
2024-09-12 15:45:19 +0300

    bağlı liste oluşturma

---
+++ src/LinkedLists/ListBuilder.java
@@ -0,0 +1,36 @@
+package LinkedLists;
+
+public class ListBuilder {
+    private Node head = null;
+    private Node tail = null;
+
+    public void add(int x) {
+        Node node = new Node();
+        node.number = x;
+        node.next = null;
+
+        if (head == null) {
+            head = node;
+            tail = node;
+            System.out.println("Zincir oluşturuldu; head = " + head.number + " tail = " + tail.number);
+        } else {
+            tail.next = node;
+            tail = node;
+            System.out.println("Yeni düğüm eklendi; tail = " + tail.number);
+        }
+    }
+
+    public void show() {
+        if (head != null) {
+            Node temp = head;
+            System.out.print("start -> ");
+            while (temp != null) {
+                System.out.print(temp.number + " -> ");
+                temp = temp.next;
+            }
+            System.out.println("end");
+        } else {
+            System.out.println("Gösterilebilecek bir zincir bulunamadı...:(");
+        }
+    }
+}
---
+++ src/LinkedLists/Main.java
@@ -0,0 +1,15 @@
+package LinkedLists;
+//Linkedlist oluşturma
+
+public class Main {
+    public static void main(String[] args) {
+        ListBuilder listBuilder = new ListBuilder();
+        listBuilder.show();
+        listBuilder.add(1);
+        listBuilder.add(2);
+        listBuilder.add(3);
+        listBuilder.add(4);
+        listBuilder.show();
+
+    }
+}
