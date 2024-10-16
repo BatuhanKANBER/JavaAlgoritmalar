e0a7a77967c3db5a1b3c793785842e0d23198c26
Batuhan KANBER <batuhankanber28@gmail.com>
2024-10-16 20:49:52 +0300

    İkili arama ağacı.

---
+++ src/BST/BinaryTree.java
@@ -0,0 +1,27 @@
+package BST;
+
+public class BinaryTree {
+    Node root;
+
+    public BinaryTree() {
+        root = null;
+    }
+
+    Node newRoot(int data) {
+        root = new Node(data);
+        return root;
+    }
+
+    Node insert(Node root, int data) {
+        if (root != null) {
+            if (data < root.data) {
+                root.left = insert(root.left, data);
+            } else {
+                root.right = insert(root.right, data);
+            }
+        } else {
+            root = newRoot(data);
+        }
+        return root;
+    }
+}
---
+++ src/BST/Main.java
@@ -0,0 +1,20 @@
+package BST;
+//İkili arama ağacı
+public class Main {
+    public static void main(String[] args) {
+        BinaryTree binaryTree = new BinaryTree();
+        binaryTree.root = binaryTree.insert(binaryTree.root, 10);
+        binaryTree.root = binaryTree.insert(binaryTree.root, 15);
+        binaryTree.root = binaryTree.insert(binaryTree.root, 8);
+        binaryTree.root = binaryTree.insert(binaryTree.root, 4);
+        binaryTree.root = binaryTree.insert(binaryTree.root, 12);
+        binaryTree.root = binaryTree.insert(binaryTree.root, 20);
+        System.out.println("root: " + binaryTree.root.data);
+        System.out.println("root sağ: " + binaryTree.root.right.data);
+        System.out.println("root sol: " + binaryTree.root.left.data);
+        System.out.println("root sol-sol: " + binaryTree.root.left.left.data);
+        System.out.println("root sağ-sol: " + binaryTree.root.right.left.data);
+        System.out.println("root sağ-sağ: " + binaryTree.root.right.right.data);
+
+    }
+}
